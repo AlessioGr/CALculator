33685647065dff8c66c65bda1ef06d359ec1a11f
Alessio Gravili <alessio@gravili.de>
2023-09-25 21:53:28 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let kcal: number;
+	let carbs: number;
+
+	let result: number;
+
+	// on change:
+	$: onChange(carbs, kcal);
+
+	function onChange(carbs: number, kcal: number) {
+		if (!carbs || !kcal) {
+			return;
+		}
+		const factor = kcal / 100;
+
+		result = carbs / factor;
+	}
+</script>
+
+<div class="inputs">
+	<input type="number" placeholder="kcal" bind:value={kcal} />
+	<input type="number" placeholder="carbs" bind:value={carbs} />
+</div>
+
+<p>Carbs per 100 kcal: <b>{result}</b></p>
+
+<style>
+	.inputs {
+		display: flex;
+		flex-direction: column;
+		width: 200px;
+	}
+
+	input {
+		margin-bottom: 1rem;
+	}
+</style>
